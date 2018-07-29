b4ccbeed6091cfe41a6c7a09792336b9e0720854
acoppola <anthony.coppola@turner.com>
2018-07-29 17:02:09 -0400

    Testing build

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # get changed files
 FILES=$(git diff --diff-filter=M --name-only origin/master..master)
+echo $FILES
 
 # from changed files, construct list of those with tests
 TEST_FILES=()
@@ -13,6 +14,7 @@ do
         TEST_FILES+=(${rel_path%.*}_tests.py)
     fi
 done
+echo $TEST_FILES
 
 # run tests against files that have been changed AND have tests associated with them
 for rel_path in ${TEST_FILES[*]}
