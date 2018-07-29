f6c89ae1b534820f50c923c6e3d515d966adf81e
acoppola <anthony.coppola@turner.com>
2018-07-29 16:19:11 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-python -m pytest ./question_2/question_2_tests.py
+FILES=$(git diff --diff-filter=M --name-only origin/master..master)
+
+for path in ${FILES[@]}
+do
+  set +e
+  python -m pytest -v $path
+  set -e
+done
+
+#python -m pytest -v ./question_2/question_2_tests.py
